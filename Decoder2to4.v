19be58f5ca744106086a3ff677355f04cf64fd04
DeepanAnbazhagan <164902865+DeepanAnbazhagan@users.noreply.github.com>
2024-03-26 17:39:34 +0530

    Update Decoder2to4.v

--- Decoder2to4.v
+++ Decoder2to4.v
@@ -20,11 +20,15 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Decoder2to4(d0,d1,d2,d3, a0, a1);
-    input a0, a1;
-    output d0,d1,d2,d3;    
-    wire an0,an1;
-                 //Write the logic for 2 to 4 decoder in Gatelevel modeling
-               
-endmodule
+  module decoder24_gate(d0,d1,d2,d3,a,b);
+  input a,b;
+  output d0,d1,d2,d3;
+  wire an0,an1;
+  not n1(an0,a);
+  not n2(an1,b);
+  and n3(d0,an0,an1);
+  and n4(d1,a,an1);
+  and n5(d2,b,an0);
+  and n6(d3,a,b);
+  endmodule
 
